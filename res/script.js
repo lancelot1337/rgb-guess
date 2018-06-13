50d148a0f7f3b272c4fd66121b25e62b4fe5ad7a
lancelot1337 <sudhanshusah2001@gmail.com>
2018-06-13 16:45:47 +0530

    Did linting on script.js

--- res/script.js
+++ res/script.js
@@ -1,59 +1,68 @@
+/*jshint esversion: 6*/
+
 /*
 var colors = [
-	"rgb(107, 91, 149)",
-	"rgb(236, 219, 84)",
-	"rgb(233, 75, 60)",
-	"rgb(111, 159, 216)",
-	"rgb(148, 71, 67)",
-	"rgb(122, 56, 100)"
+	'rgb(107, 91, 149)',
+	'rgb(236, 219, 84)',
+	'rgb(233, 75, 60)',
+	'rgb(111, 159, 216)',
+	'rgb(148, 71, 67)',
+	'rgb(122, 56, 100)'
 	];
 */
+
 var colors = colorsArrGen(6);
+
 // var correctColor = colors[3];
 var correctColor = getRandSeedColor();
-var squares = document.querySelectorAll(".square");
-var rgbHead = document.getElementById("rgbHead");
-var gamePrompt = document.querySelector("#gamePrompt");
-var H1 = document.getElementById("H1");
-var resetButton = document.getElementById("resetButton");
+var squares = document.querySelectorAll('.square');
+var rgbHead = document.getElementById('rgbHead');
+var gamePrompt = document.querySelector('#gamePrompt');
+var H1 = document.getElementById('H1');
+var resetButton = document.getElementById('resetButton');
 
-resetButton.addEventListener("click",() => {
+resetButton.addEventListener('click', () => {
 	//Update the resetButton
-	resetButton.textContent = "New Colors";
+	resetButton.textContent = 'New Colors';
+
 	//Gen a new colors array
 	colors = colorsArrGen(6);
+
 	//Gen a new correctColor
 	correctColor = getRandSeedColor();
+
 	//Update the background of squares
-	for(var i = 0; i < colors.length; ++i){
+	for (var i = 0; i < colors.length; ++i) {
 		squares[i].style.background = colors[i];
 	}
+
 	//Update the header text
 	rgbHead.textContent = correctColor.toUpperCase();
+
 	//Update the header background
 	H1.style.background = '#232323';
 });
 
 rgbHead.textContent = correctColor.toUpperCase();
-for(var i = 0; i < colors.length; ++i){
+for (var i = 0; i < colors.length; ++i) {
 	//Add colors to the boxes
 	squares[i].style.background = colors[i];
 
 	//Add click listeners
-	squares[i].addEventListener("click",function(){
+	squares[i].addEventListener('click', function () {
 		//Grab the color of the clicked box
 		var clickedColor = this.style.background;
 
 		//Check if the colors are same
-		if(clickedColor === correctColor){
-			gamePrompt.textContent = "Correct!";
+		if (clickedColor === correctColor) {
+			gamePrompt.textContent = 'Correct!';
 			changeColors(clickedColor);
 			H1.style.background = clickedColor;
-			resetButton.textContent = "Play Again";
+			resetButton.textContent = 'Play Again';
 		}
 		else{
-			this.style.background = "#232323";
-			gamePrompt.textContent = "Try Again!";
+			this.style.background = '#232323';
+			gamePrompt.textContent = 'Try Again!';
 		}
 	});
 }
@@ -84,5 +93,5 @@ function randColorGenerator(){
 	var r = Math.floor(Math.random() * 256);
 	var g = Math.floor(Math.random() * 256);
 	var b = Math.floor(Math.random() * 256);
-	return ("rgb(" + r + ", " + g + ", " + b + ")");
+	return ('rgb(' + r + ', ' + g + ', ' + b + ')');
 }
